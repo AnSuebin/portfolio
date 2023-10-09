3046cd0f474f0b73e9463c61108005c0819d22a1
Suebin An <jejupalette@naver.com>
2023-10-09 21:31:51 +0900

    styles : 메인 페이지 background 추가

--- src/components/home/HomeIntro.tsx
+++ src/components/home/HomeIntro.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 const HomeIntro = () => {
   return (
-    <main className="flex items-center justify-center h-screen">
-      <h1 className="leading-normal font-primary">
-        안녕하세요. <br />
-        주니어 프론트엔드 개발자 <br />
-        안수빈입니다.
-      </h1>
-    </main>
+    <div className="bg-repeat bg-pattern">
+      <main className="flex items-center justify-center h-screen">
+        <h1 className="leading-normal font-primary">
+          리트리버를 꿈꾸는
+          <br />
+          프론트엔드 개발자
+          <br />
+          안수빈입니다.
+        </h1>
+      </main>
+    </div>
   );
 };
 
