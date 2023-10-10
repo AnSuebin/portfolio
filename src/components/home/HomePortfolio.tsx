06914250d88457870197b2e8e7c92b3ba9864b15
Suebin An <jejupalette@naver.com>
2023-10-10 23:14:30 +0900

    feat : 메인 페이지 자동 스크롤 구현

--- src/components/home/HomePortfolio.tsx
+++ src/components/home/HomePortfolio.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+'use client';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import HomePortfolioBox from './portfolio/HomePortfolioBox';
 import { portfolioList } from '@/utils/portfolio';
+import { useInView } from 'react-intersection-observer';
 
 const HomePortfolio = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [viewRef, inView] = useInView();
+
+  useEffect(() => {
+    if (!!scrollRef.current && inView) {
+      scrollRef.current.scrollIntoView({
+        behavior: 'auto',
+        block: 'start',
+      });
+    }
+  }, [inView, scrollRef]);
+
   return (
-    <main className="flex items-center justify-center h-screen">
+    <main className="flex items-center justify-center h-screen" ref={scrollRef}>
       <section className="flex px-[50px] py-[180px] gap-[20px] flex-row h-full w-full">
-        <section className="flex flex-col flex-1 h-full px-5 py-10 border font-primary">
+        <section
+          className="flex flex-col flex-1 h-full px-5 py-10 border font-primary"
+          ref={viewRef}
+        >
           portfolio
         </section>
+
         {portfolioList.map((portfolio, index) => (
           <HomePortfolioBox
             key={`Home-portfolio-block-${portfolio.id}`}
--- src/components/home/HomeSummary.tsx
+++ src/components/home/HomeSummary.tsx
@@ -1,16 +1,32 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import profileImage from '../../../public/images/profile.png';
+import { useInView } from 'react-intersection-observer';
 
 const HomeSummary = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [viewRef, inView] = useInView();
+
+  useEffect(() => {
+    if (!!scrollRef.current && inView) {
+      scrollRef.current.scrollIntoView({
+        behavior: 'auto',
+        block: 'start',
+      });
+    }
+  }, [inView, scrollRef]);
+
   return (
-    <main className="flex items-center justify-center h-screen ">
+    <main className="flex items-center justify-center h-screen" ref={scrollRef}>
       <section className="flex items-center justify-between px-[100px] py-[180px] h-full w-full">
         <Image src={profileImage} width={400} height={400} alt="profile" />
         <div className="ml-10">
-          <h3 className="mb-10 text-2xl font-primary">
+          <h3 className="mb-10 text-2xl font-primary" ref={viewRef}>
             리트리버 프론트엔드 개발자_ 안수빈
           </h3>
+
           <article>
             리트리버는 시각 안내 도우미를 꿈꾸는 견종입니다. <br />저 또한
             누군가의 따뜻한 눈이 되는 프론트엔드 개발자를 꿈꾸며 성장합니다.
