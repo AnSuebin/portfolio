19b3603e15d5af35fb59cc8fadd0e043a2347155
Suebin An <jejupalette@naver.com>
2023-10-13 22:35:50 +0900

    styles : 메인 페이지 텍스트 애니메이션

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomeHeader = () => {
   return (
-    <header className="fixed w-full max-w-screen-xl top-0 cursor-pointer flex border-b-[1.5px] border-lightGray bg-background">
+    <header className="fixed z-50 w-full max-w-screen-xl top-0 cursor-pointer flex border-b-[1.5px] border-lightGray bg-background">
       <Link href={'/'}>
         <div className="mx-5 mt-5 mb-3">
           <div className="text-lg font-extrabold">ANSOOBIN</div>
