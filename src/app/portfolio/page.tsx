674232306b0c714b24876d6da71c764fd4783a52
Suebin An <jejupalette@naver.com>
2023-10-13 23:14:13 +0900

    remove : 불필요 코드 제거

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -3,12 +3,6 @@
 import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 
-import { PortfolioIdType } from '@/apis/Home.type';
-import { portfolioDetail } from '@/utils/portfolio';
-import PortfolioDetailBox from '@/components/portfolio/PortfolioDetailBox';
-import PortfolioHeader from '@/components/portfolio/layout/PortfolioHeader';
-import PortfolioMenu from '@/components/portfolio/layout/PortfolioMenu';
-
 const PortfolioPage = () => {
   const router = useRouter();
 
